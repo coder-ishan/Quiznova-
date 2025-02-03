5dd2a98e9d2304e5d2581f85ff33b23749d198cb
AbhayK200417 <bhorayatabhay2004@gmail.com>
2025-02-03 20:24:47 +0530

    Created a query for local backend to fetch the responses and added a page to input the data from the users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold mb-6">Welcome to the Quiz App</h1>
-      <Link href="/quiz">
+      <Link href="/studentInfo">
         <div className="px-4 py-2 bg-blue-500 text-white rounded">Start Quiz</div>
       </Link>
     </div>
